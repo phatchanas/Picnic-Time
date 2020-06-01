26f276b34c217acf484f99be1af069c0a6086f51
ronjanackblad <ronja.nackblad@hotmail.com>
2020-06-01 16:17:03 +0200

    adds dropdown mennu for recipe

--- my-app/src/components/recipe.js
+++ my-app/src/components/recipe.js
@@ -37,19 +37,48 @@ const H2 = styled.h2`
 font-family: 'Chelsea Market', cursive;
 `;
 
+const Button = styled.button`
+  font-family: 'Chelsea Market', cursive; 
+  background-color: white; 
+  border: 4px solid #585c6e;
+  color: black;
+  margin-top: 20px;
+  padding: 10px 30px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  transition-duration: 1s;
+  @media only screen and (max-width: 600px) {
+    width:50%;
+
+  }
+  `;
 
 
 
- export default (props) => {
-     return(
+export default (props) => {
+    const [isOpen, toggleIsOpen] = React.useState(false);
+
+    const handleButtonClick = (e) => {
+        if (isOpen === false) {
+            toggleIsOpen(true)
+        } else {
+            toggleIsOpen(false)
+        }
+    }
+
+    return (
         <Div>
             <H2>
                 {props.recipe.recipe.label}
             </H2>
-            <Imgfood src={props.recipe.recipe.image} alt="food"/>
-            <Ul>
-                {props.recipe.recipe.ingredients.map((item, i) => <li key={i}>{item.text}</li>)}
-            </Ul>
-         </Div>
-     )
- }
+            <Imgfood src={props.recipe.recipe.image} alt="food" />
+            <div>
+                <Button onClick={handleButtonClick}>Visa recept</Button>
+                {isOpen ? <Ul>
+                    {props.recipe.recipe.ingredients.map((item, i) => <li key={i}>{item.text}</li>)}
+                </Ul> : <p></p>}
+            </div>
+        </Div>
+    )
+}
